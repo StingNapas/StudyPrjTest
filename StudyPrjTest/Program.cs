9328d9c9c218808d2f09025bcf1600072be4352d
Титовец Сергей <Titovets_SA@corp.akelon.com>
2022-03-28 08:37:12 +0300

    Добавьте файлы проекта.

---
+++ StudyPrjTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudyPrjTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a + b);
+            List 
+            //Method1(ref a);
+        }
+
+        public static void Method1(ref int t)
+        {
+            t += 1;
+            Console.WriteLine(t);
+        }
+
+        public static void Test1()
+        {
+            int[] arr = { 1, 2, 3 };
+            ref int a = ref arr[0];
+        }
+
+        public static void TestMeth() => Console.WriteLine("ss");
+
+    }
+}
